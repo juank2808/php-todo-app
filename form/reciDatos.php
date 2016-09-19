c0733dc3a9ed3f160bfd78eacf0314add0dc2e57
juankcol <jamayafl15dw@ikzubirimanteo.com>
2016-09-19 18:44:31 +0000

    minicorrecion

--- form/reciDatos.php
+++ form/reciDatos.php
@@ -4,9 +4,9 @@ $pass=$_POST['pass'];
 
 
 
-$consulta=mysql_query("select user, password from usuarios where user =".$user."and password ="$pass";");
+$consulta=mysql_query("select user, password from usuarios where user =" . $user. "and password =".$pass";");
 
-if(my_sql_num_rows($consulta)==10){
+if(my_sql_num_rows($consulta)==1){
     
     
 }else{
